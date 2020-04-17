ced775ab25d3afc82498a4120c104df16697afd5
john <jc5808@stern.nyu.edu>
2020-04-17 11:23:21 -0400

    fixed the index test for the UI changes

--- tests/test_service.py
+++ tests/test_service.py
@@ -76,8 +76,9 @@ class TestPromotionServer(TestCase):
     ######################################################################
     def test_index(self):
         """ Test index call """
-        resp = self.app.get("/")
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        resp = self.app.get('/')
+        self.assertEqual(resp.status_code, HTTP_200_OK)
+        self.assertIn(b'Promotion Demo REST API Service', resp.data)
 
 
     def test_create_promotion(self):
